a22aa99d4de9b6a3bd961fda55c834934c51fa76
unknown <taras.tsupryk@gmail.com>
2021-09-19 22:04:35 +0300

    make public

--- src/app.js
+++ src/app.js
@@ -3,10 +3,17 @@ import GroupRouter from "./components/groups/group.router.js";
 import TeacherRouter from "./components/teachers/teacher.router.js";
 import DisciplineRouter from "./components/disciplines/discipline.router.js";
 import AudienceRouter from "./components/audience/audience.router.js";
-import FacultyRouter from './components/faculties/faculty.router.js'
+import FacultyRouter from "./components/faculties/faculty.router.js";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 const API_URL = "/api";
 app.use(API_URL, GroupRouter);
